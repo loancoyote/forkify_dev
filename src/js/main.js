36f03048aafbe1a95807aea17cce9ad6cbd5e6c4
yamato <takayuki.yamato@gmail.com>
2022-08-28 23:12:49 +0900

    git init　です

---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+// import controller from './controller.js';
+import '../scss/main.scss';
+
+const recipeContainer = document.querySelector('.recipe');
+
+const timeout = function (s) {
+  return new Promise(function (_, reject) {
+    setTimeout(function () {
+      reject(new Error(`Request took too long! Timeout after ${s} second`));
+    }, s * 1000);
+  });
+};
---
+++ webpack.config.js
@@ -0,0 +1,86 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'source-map',
+  entry: './src/js/main.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: './js/main.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                ['@babel/preset-env', { targets: '> 0.25%, not dead' }],
+              ],
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(css|sass|scss)/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: false,
+            },
+          },
+          {
+            loader: 'sass-loader',
+          },
+        ],
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg|ico)/,
+        type: 'asset/resource',
+        generator: {
+          filename: (pathData) => {
+            const filePath = path
+              .dirname(pathData.filename)
+              .split('/')
+              .slice(1)
+              .join('/');
+            return `${filePath}/[name]-[contenthash][ext]`;
+          },
+        },
+        use: [
+          {
+            loader: 'image-webpack-loader',
+          },
+        ],
+      },
+      {
+        test: /\.(html)$/,
+        use: [
+          {
+            loader: 'html-loader',
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: './css/style.css',
+    }),
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      filename: 'index.html',
+    }),
+    new CleanWebpackPlugin(),
+  ],
+};
